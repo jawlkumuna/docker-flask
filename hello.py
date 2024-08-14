bbfc5cd84c9571103e18496e7064f812de51a42e
András Seres <91373059+jawlkumuna@users.noreply.github.com>
2024-08-14 19:09:33 +0200

    fix stuff

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
+"""Main webapp module"""
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -6,5 +8,5 @@ def hello():
     return "Hello World!"
 
 @app.route("/morning")
-def hello():
+def morning():
     return "Good morning!"
